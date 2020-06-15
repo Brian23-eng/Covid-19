bd4fd882708b59ce198e788c7605548766f94168
brian23-eng <b.odhiambo.bo@gmail.com>
2020-06-15 13:44:07 +0300

    create a function with ajax as an object to get request from our API

--- scripts.js
+++ scripts.js
@@ -3,3 +3,14 @@ $(document).ready(function() {
         alert("This is  header")
     });
 });
+
+
+$(function() {
+    $.ajax({
+        type: 'GET',
+        url: 'https://covid19.mathdro.id/api',
+        success: function(data) {
+            console.log('success', data);
+        }
+    });
+});
