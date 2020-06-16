830b69da15114800f31fed5699c6b31e925b7f4d
brian23-eng <b.odhiambo.bo@gmail.com>
2020-06-16 18:29:04 +0300

    destructure the objects

--- scripts.js
+++ scripts.js
@@ -1,16 +1,14 @@
-$(document).ready(function() {
-    $("h1").click(function() {
-        alert("This is  header")
-    });
-});
-
-
 $(function() {
+
+    var $data = $("#rrecover")
     $.ajax({
         type: 'GET',
         url: 'https://covid19.mathdro.id/api',
         success: function(data) {
-            console.log('success', data);
+            $.each(data, function(i, data) {
+
+                $data.append('<li> ' + data + ' </li>')
+            })
         }
     });
 });
