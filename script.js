7fcb7083299a3b88bb513aca00a7787a96cfa902
brian23-eng <b.odhiambo.bo@gmail.com>
2020-06-18 22:20:41 +0300

    style the webpage

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+//navbar
+$(function() {
+    var header = $(".navbar");
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 50) {
+            header.addClass("scrolled");
+        } else {
+            header.removeClass("scrolled");
+        }
+    });
+});
+
+
+
+
 //Kenya
 document.getElementById("button").addEventListener('click', loadText);
 
